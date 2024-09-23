b8af624aa4b9a255221512bf2778848ac81920c5
Joshua-Yel <jutribiana@gmail.com>
2024-09-24 00:25:35 +0800

    home page and routerlink update

--- mamarits/src/App.js
+++ mamarits/src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Home from "./components/Home";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Logo from "./assets/mamaritsLogo.jpg";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+      <div className="container-fluid">
+        <div className="d-flex align-items-center">
+          <img
+            src={Logo}
+            alt="FoodHub Logo"
+            className="navbar-brand"
+            style={{ width: "100px", marginRight: "25px" }}
+          />
+          <div className="navbar-nav mr-auto d-flex flex-row">
+            <Nav.Link
+              as={Link}
+              to="/"
+              className="nav-item nav-link"
+            >
+              Find Food
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/about"
+              className="nav-item nav-link"
+            >
+              About Us
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/contact"
+              className="nav-item nav-link"
+            >
+              Contact
+            </Nav.Link>
+          </div>
+        </div>
+        <div className="navbar-nav ml-auto d-flex flex-row">
+          <Nav.Link
+            as={Link}
+            to="/login"
+            className="nav-item nav-link"
+          >
+            Login
+          </Nav.Link>
+          <Nav.Link
+            as={Link}
+            to="/signup"
+            className="nav-item nav-link"
+          >
+            Signup
+          </Nav.Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        {/* <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+        <Route
+          path="/counter"
+          element={<Counter />}
+        />
+        <Route
+          path="*"
+          element={<Error />}
+        /> */}
+      </Routes>
+    </Router>
   );
 }
 
--- mamarits/src/index.js
+++ mamarits/src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
   </React.StrictMode>
 );
 
